d22e0af5ee651557dc2e6b73c87b70393684d1a5
Vinicius Emanoel Andrade <viniciusemanoelandrade@hotmail.com>
2019-11-17 00:56:00 -0300

    melhorias no estilo; separando o algoritmo em funções; iniciando a representação visual da máquina de estados

--- scripts/script.js
+++ scripts/script.js
@@ -5,78 +5,89 @@ $('#token').keypress(function (e)
     if (e.which == 13) 
     {
         let elemento = $(this);
-        let palavras = elemento.val().split(' ');
-        $.each(palavras, function (index, token) 
-        {
-            alfabeto.push(token);
-        });
+        let tokens = elemento.val().split(' ');
         elemento.val('');
-
-        // Criando os estados        
-        var estado = 0;
-        for(var i=0; i<alfabeto.length; i++) 
-        {
-            let estado_atual = 0;
-            let palavra = alfabeto[i];
-            for(var j=0; j<palavra.length; j++) 
-            {
-                var letra = palavra[j];
-                if(typeof estados[estado_atual][letra] === 'undefined') 
-                {
-                    let proximo_estado = estado + 1;
-                    estados[estado_atual][letra] = proximo_estado;
-                    estados[proximo_estado] = [];
-                    estado = estado_atual = proximo_estado;
-                } 
-                else 
-                {
-                    estado_atual = estados[estado_atual][letra];
-                }
-    
-                if(j == palavra.length - 1) 
-                {
-                    estados[estado_atual]['final'] = true;
-                }
-            }
-        }
-
-        console.log(estados.length);
-
-        // Montando a tabela
-        var conteudo = '';
-        conteudo += '<thead><tr>';
-        var i = 'a'.charCodeAt(0); j = 'z'.charCodeAt(0);
-        for (; i <= j; ++i) 
-        {            
-            var letra = String.fromCharCode(i);
-            conteudo += '<th>' +letra+ '</th>';   
-        }        
-        conteudo += '</tr></thead>';
-        for (let h = 0; h < estados.length; h++) {
-            conteudo += '<tr>';
-            const linha = estados[h];
-            var i = 'a'.charCodeAt(0); j = 'z'.charCodeAt(0);
-            conteudo += '<td>q'+h+'</td>';
-            for (; i <= j; ++i)
-            {
-                var letra = String.fromCharCode(i);
-                if(typeof estados[h][letra] !== 'undefined') {
-                    conteudo += '<td>q'+ estados[h][letra] +'</td>';
-                } else {
-                    conteudo += '<td>-</td>';
-                }
-            }
-            conteudo += '</tr>';
-        }
-        $('#tabela').html(conteudo);
+        
+        criar_alfabeto(tokens);
+        identificar_estados_do_automato();
+        desenhar_tabela();
      }
 });
 
-function gerar_letras() 
-{
-    var i = 'a'.charCodeAt(0); j = 'z'.charCodeAt(0);
-    for (; i <= j; ++i) 
+function criar_alfabeto(tokens) {
+    $.each(tokens, function (index, token) 
     {
-        var letra = String.fromCharCode(i);
+        alfabeto.push(token);
+        $('#alfabeto').append(
+            '<span class="badge badge-pill badge-primary palavras">'+token+'</span>'
+        );
+    });    
+}
+
+function identificar_estados_do_automato()
+{
+    var estado = 0;
+    for(var i=0; i<alfabeto.length; i++) 
+    {
+        let estado_atual = 0;
+        let palavra = alfabeto[i];
+        for(var j=0; j<palavra.length; j++) 
+        {
+            var letra = palavra[j];
+            if(typeof estados[estado_atual][letra] === 'undefined') 
+            {
+                let proximo_estado = estado + 1;
+                estados[estado_atual][letra] = proximo_estado;
+                estados[proximo_estado] = [];
+                estado = estado_atual = proximo_estado;
+            } 
+            else 
+            {
+                estado_atual = estados[estado_atual][letra];
+            }
+
+            if(j == palavra.length - 1) 
+            {
+                estados[estado_atual]['final'] = true;
+            }
+        }
     }
 }
+
+function desenhar_tabela() {
+    var conteudo = '';
+    conteudo += '<thead><tr>';
+    conteudo += '<td>-</td>';
+    var i = 'a'.charCodeAt(0); j = 'z'.charCodeAt(0);
+    for (; i <= j; ++i) 
+    {            
+        var letra = String.fromCharCode(i);
+        conteudo += '<th>' +letra+ '</th>';   
+    }        
+    conteudo += '</tr></thead>';
+    for (let h = 0; h < estados.length; h++) 
+    {
+        conteudo += '<tr>';
+        const linha = estados[h];
+        var i = 'a'.charCodeAt(0); j = 'z'.charCodeAt(0);
+        conteudo += '<td><strong>q'+h+'</strong></td>';
+        for (; i <= j; ++i)
+        {
+            var letra = String.fromCharCode(i);
+            if(typeof estados[h][letra] !== 'undefined') {
+                conteudo += '<td>q'+ estados[h][letra] +'</td>';
+            } else {
+                conteudo += '<td>-</td>';
+            }
+        }
+        conteudo += '</tr>';
+    }
+    $('#tabela').html(conteudo);
+}
+
+$('#palavra').keypress(function (e) 
+{
+    var estado = $(this).val().length;
+    var letra = String.fromCharCode(e.which);
+    var proximo_estado = estados[estado][letra];    
+});
